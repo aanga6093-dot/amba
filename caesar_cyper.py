be91bd8d45b5ba7b0aefc586e5267cb2d8d6cbb9
aanga6093-dot <aanga6093@gmail.com>
2026-06-24 09:21:44 +0700

    Isi file nya

--- caesar_cyper.py
+++ caesar_cyper.py
@@ -1,1 +1,17 @@
+import string
+key = 20
 
+text = ""
+
+kata_baru = ""
+
+#esi na abcdefgh.. terus 
+kata = string.ascii_lowercase + " " + string.ascii_lowercase + " "
+
+for chat in text:
+    if chat in kata:
+        
+        indet = kata.index(2,chat) + key
+        kata_baru += kata[indet]
+    
+print(kata_baru)    
